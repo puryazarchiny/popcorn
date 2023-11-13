03082fee233ffdce05ab53cef87418a108fc66b7
Purya Zarchiny <puryazarchiny@gmail.com>
2023-11-13 11:41:30 +0330

    Fix

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,6 +1,8 @@
 import { BookmarkIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
 
+import githubMark from "@/assets/icons/github-mark.svg";
+
 export function Nav() {
   return (
     <nav>
@@ -20,11 +22,7 @@ export function Nav() {
             target="_blank"
             className="flex h-9 w-9 cursor-pointer items-center justify-center rounded bg-slate-200 hover:bg-slate-100"
           >
-            <img
-              src="src/assets/icons/github-mark.svg"
-              alt="Invertocat logo"
-              className="w-6"
-            />
+            <img src={githubMark} alt="Invertocat logo" className="w-6" />
           </a>
         </li>
       </ul>
